1246b317a52a76cd55ec049778ba444b3ed280e0
James Heggs <james.heggs@techreturners.com>
2021-01-16 15:34:16 +0000

    Successfully displaying tasks via GET request

--- Handler.cs
+++ Handler.cs
@@ -1,24 +1,32 @@
 using Amazon.Lambda.Core;
+using System.Collections;
 
 [assembly:LambdaSerializer(typeof(Amazon.Lambda.Serialization.SystemTextJson.DefaultLambdaJsonSerializer))]
 namespace AwsDotnetCsharp
 {
     public class Handler
     {
-       public Response Hello(Request request)
+       public ArrayList Hello(Request request)
        {
-           return new Response("Hello Tech Returners!", request);
+          ArrayList tasks = new ArrayList();
+          Task t1 = new Task("abc1234", "Buy the milk", false);
+          Task t2 = new Task("abc4567", "Pick up newspaper", false);
+          tasks.Add(t1);
+          tasks.Add(t2);
+          return tasks;
        }
     }
 
-    public class Response
+    public class Task
     {
-      public string Message {get; set;}
-      public Request Request {get; set;}
+      public string TaskId {get;}
+      public string Description {get;}
+      public bool Completed {get;}
 
-      public Response(string message, Request request){
-        Message = message;
-        Request = request;
+      public Task(string taskId, string description, bool completed){
+        TaskId = taskId;
+        Description = description;
+        Completed = completed;
       }
     }
 
